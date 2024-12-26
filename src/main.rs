174ece81699b674d62e1198d1ebeab941593bc14
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2024-12-26 16:27:59 +0000

    first commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::{io::{Read, Write}, net::TcpListener};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                let mut buf = [0; 512];
+
+                loop {
+                    let read_count = stream.read(&mut buf).unwrap();  
+
+                    if read_count == 0 { 
+                        break;
+                    }
+
+                    stream.write_all(b"+PONG\r\n").unwrap();
+                }
+            }
+            Err(e) => {
+                println!("Error: {}", e)
+            }
+        }
+    }
+}
